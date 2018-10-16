5feb96898c1fa215320fd5f1cf175fd883d2ed51
tnwjd4623 <33441654+tnwjd4623@users.noreply.github.com>
2018-10-16 20:34:45 +0900

    Update insertionSort.java

--- Sort/insertionSort.java
+++ Sort/insertionSort.java
@@ -27,20 +27,5 @@ public class insertionSort {
 				number[j+1] = tmp;
 		}
 	}
-	public void printArray() {
-		System.out.print("[");
-		for(int i = 0; i<size-1; i++) {
-			System.out.print(number[i]+", ");
-		}
-		System.out.println(number[size-1]+" ]");
-	}
 	
-	public void printFile(File file) throws IOException{
-		FileWriter fw = new FileWriter(file);
-		for(int i = 0; i<size-1; i++) {
-			fw.write(number[i]+",");
-		}
-		fw.write(number[size-1]+"");
-		fw.close();
-	}
 }
