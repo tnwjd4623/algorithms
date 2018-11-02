9a1416d9f74c5e95ca7a7ee4f20fa618c4cbc673
tnwjd4623 <33441654+tnwjd4623@users.noreply.github.com>
2018-11-02 19:11:05 +0900

    Add files via upload

---
+++ Karatsuba.java
@@ -0,0 +1,39 @@
+package hw06;
+
+
+public class Karatsuba {
+	private int threshold;
+	
+	public Karatsuba() {
+		this.threshold = 3;
+	}
+	public long mul(long x, long y) {
+		int n = getThreshold(x);		//x와 y의 자리수가 똑같음?
+		
+		if(n <= threshold) {
+			return x*y;
+		}
+		
+		int m = n/2;
+		long Bm = (long)Math.pow(10, m);
+		
+		long x1 = x/Bm;
+		long x0 = x-(x1*Bm);
+		long y1 = y / Bm;
+		long y0 = y - (y1*Bm);
+		
+		long z2 = mul(x1,y1);
+		long z0 = mul(x0, y0);
+		long z1 = mul(x1+x0,y1+y0) - z2-z0;
+		
+		return (z2*(long)Math.pow(10, 2*m)) + (z1*(long)Math.pow(10,  m)) + z0;
+	}
+	public int getThreshold(long n) {
+		int count = 0;
+		while(n !=0 ) {
+			count++;
+			n /= 10;
+		}
+		return count;
+	}
+}
